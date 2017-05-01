78cc1a30057f8af1d36e4a2bd6f49270b0f377b7
Ian Ramirez-España <ianespana@hotmail.com>
2017-05-01 10:59:49 -0500

    Added - Initial preview drawing, input variables

--- inc/drawing.js
+++ inc/drawing.js
@@ -20,26 +20,49 @@ function downloadFile(fileName, fileText) {
 	document.body.removeChild(element);
 }
 
-function generateModel(midiData, holeWidth, holeLength) {	
+function generateModel(midiData, holeWidth, holeLength, horizontalMargin, verticalMargin) {	
 	var notesAgainstTime = processData(midiData);			
 	var punchHoles = [];
 	var model = { models: {}};
+	var biggestY = 0;
+	var biggestX = 0;
+	var rectX = 0;
+	var rectY = 0;
+	
 	for (var i=0; i<notesAgainstTime.length; i++) {
 		var note = notesAgainstTime[i];
 		var oval = new makerjs.models.Oval(holeLength,holeWidth);
 		oval.units = makerjs.unitType.Millimeter;
-		oval.origin = [note.x-(holeLength-(holeWidth/2)), note.y-(holeWidth/2)];
+		oval.origin = [(note.x-(holeLength-(holeWidth/2)))+(horizontalMargin*1 + holeLength/2), (note.y-(holeWidth/2))+(verticalMargin*1 + holeWidth/2)];
 		punchHoles.push(oval);
+		
+		if (oval.origin[0] > biggestX) {
+			biggestX = oval.origin[0];
+		}
+		
+		if (oval.origin[1] > biggestY) {
+			biggestY = oval.origin[1];
+		}
 	}
+	rectX = (biggestX + horizontalMargin*1 + 2*holeLength/2);
+	rectY = (document.getElementById("strip-height").value);
+	
+	console.log(biggestX + ", " + biggestY);
+	console.log(holeLength + ", " + holeWidth);
+	console.log(rectX + ", " + rectY);
+	
+	var rectangle = new makerjs.models.Rectangle(rectX, rectY*1);
+	rectangle.units = makerjs.unitType.Millimeter;
+	
+	punchHoles.push(rectangle);
 	model.models = punchHoles;
 	return model;
 }
 
 function prepareFiles(midiData) {
-	var model = generateModel(midiData, 1.5, 2);
-	svgFile = null;
-	dxfFile = null;
-
+	var model = generateModel(midiData, document.getElementById("hole-height").value, document.getElementById("hole-width").value, document.getElementById("horizontal-margins").value, document.getElementById("vertical-margins").value);
+	
 	fileTexts["svg"] = makerjs.exporter.toSVG(model, {units: makerjs.unitType.Millimeter, useSvgPathOnly: false});
 	fileTexts["dxf"] = makerjs.exporter.toDXF(model);	
+	document.getElementById("preview-box").innerHTML = fileTexts["svg"];
 }
