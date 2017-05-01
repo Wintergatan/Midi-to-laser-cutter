707542e54ade5a468d0da99e5fd636bc5ca6f12a
Ian Ramirez-España <ianespana@hotmail.com>
2017-05-01 13:24:02 -0500

    Multiple changes
    Fixed - Broken model exports
    Added - Plain text output, strip height input, rectangle shape,
    different units

--- inc/drawing.js
+++ inc/drawing.js
@@ -1,6 +1,7 @@
 var fileTexts = {
 	"svg": null,
-	"dxf": null
+	"dxf": null,
+	"txt": null
 };
 
 function downloadFile(fileName, fileText) {
@@ -20,49 +21,80 @@ function downloadFile(fileName, fileText) {
 	document.body.removeChild(element);
 }
 
-function generateModel(midiData, holeWidth, holeLength, horizontalMargin, verticalMargin) {	
+function generateModel(midiData, holeWidth, holeHeight, horizontalMargin, verticalMargin, stripHeight, units) {	
 	var notesAgainstTime = processData(midiData);			
 	var punchHoles = [];
+	var origins = "";
 	var model = { models: {}};
 	var biggestY = 0;
 	var biggestX = 0;
 	var rectX = 0;
 	var rectY = 0;
 	
-	for (var i=0; i<notesAgainstTime.length; i++) {
-		var note = notesAgainstTime[i];
-		var oval = new makerjs.models.Oval(holeLength,holeWidth);
-		oval.units = makerjs.unitType.Millimeter;
-		oval.origin = [(note.x-(holeLength-(holeWidth/2)))+(horizontalMargin*1 + holeLength/2), (note.y-(holeWidth/2))+(verticalMargin*1 + holeWidth/2)];
-		punchHoles.push(oval);
-		
-		if (oval.origin[0] > biggestX) {
-			biggestX = oval.origin[0];
+	
+	if (document.getElementById("hole-shape").value == "Square"){
+		for (var i=0; i<notesAgainstTime.length; i++) {
+			var note = notesAgainstTime[i];
+			var rectangle = new makerjs.models.Rectangle(holeWidth,holeHeight);
+			rectangle.units = units;
+			rectangle.origin = [(note.x-(holeWidth-(holeHeight/2)))+(horizontalMargin + holeWidth/2), (note.y-(holeHeight/2))+(verticalMargin + holeHeight/2)];
+			punchHoles.push(rectangle);
+			origins = origins + rectangle.origin[0] + "," + rectangle.origin[1] + "\n";
+			
+			if (rectangle.origin[0] > biggestX) {
+				biggestX = rectangle.origin[0];
+			}
+			
+			if (rectangle.origin[1] > biggestY) {
+				biggestY = rectangle.origin[1];
+			}
 		}
-		
-		if (oval.origin[1] > biggestY) {
-			biggestY = oval.origin[1];
+	} else {
+		for (var i=0; i<notesAgainstTime.length; i++) {
+			var note = notesAgainstTime[i];
+			var oval = new makerjs.models.Oval(holeWidth,holeHeight);
+			oval.units = units;
+			oval.origin = [(note.x-(holeWidth-(holeHeight/2)))+(horizontalMargin + holeWidth/2), (note.y-(holeHeight/2))+(verticalMargin + holeHeight/2)];
+			punchHoles.push(oval);
+			origins = origins + oval.origin[0] + "," + oval.origin[1] + "\n";
+			
+			if (oval.origin[0] > biggestX) {
+				biggestX = oval.origin[0];
+			}
+			
+			if (oval.origin[1] > biggestY) {
+				biggestY = oval.origin[1];
+			}
 		}
 	}
-	rectX = (biggestX + horizontalMargin*1 + 2*holeLength/2);
-	rectY = (document.getElementById("strip-height").value);
+	rectX = (biggestX + horizontalMargin + holeWidth);
+	rectY = stripHeight;
 	
-	console.log(biggestX + ", " + biggestY);
-	console.log(holeLength + ", " + holeWidth);
-	console.log(rectX + ", " + rectY);
-	
-	var rectangle = new makerjs.models.Rectangle(rectX, rectY*1);
-	rectangle.units = makerjs.unitType.Millimeter;
+	var rectangle = new makerjs.models.Rectangle(rectX, rectY);
+	rectangle.units = units;
 	
 	punchHoles.push(rectangle);
 	model.models = punchHoles;
-	return model;
+	return [model, origins];
 }
 
 function prepareFiles(midiData) {
-	var model = generateModel(midiData, document.getElementById("hole-height").value, document.getElementById("hole-width").value, document.getElementById("horizontal-margins").value, document.getElementById("vertical-margins").value);
+	var units = makerjs.unitType.Millimeter;
+	if (document.getElementById("units").value == "Inches"){
+		units = makerjs.unitType.Inch;
+	}
+	var holeWidth = parseFloat(document.getElementById("hole-width").value);
+	var holeHeight = parseFloat(document.getElementById("hole-height").value);
+	var hMargin = parseFloat(document.getElementById("horizontal-margins").value);
+	var vMargin = parseFloat(document.getElementById("vertical-margins").value);
+	var stripHeight = parseFloat(document.getElementById("strip-height").value);
 	
-	fileTexts["svg"] = makerjs.exporter.toSVG(model, {units: makerjs.unitType.Millimeter, useSvgPathOnly: false});
-	fileTexts["dxf"] = makerjs.exporter.toDXF(model);	
+	var returnArray = generateModel(midiData, holeWidth, holeHeight, hMargin, vMargin, stripHeight, units);
+	var model = returnArray[0];
+	var origins = returnArray[1];
+	
+	fileTexts["svg"] = makerjs.exporter.toSVG(model, {units: units, useSvgPathOnly: false});
+	fileTexts["dxf"] = makerjs.exporter.toDXF(model, {units : units});
+	fileTexts["txt"] = origins;
 	document.getElementById("preview-box").innerHTML = fileTexts["svg"];
 }
