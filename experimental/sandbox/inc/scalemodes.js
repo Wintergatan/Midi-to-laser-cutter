d3b1dba4ffca8da771f6754e9dacc8495663bd21
Andrew Craigie <andrew@yuleblinker.com>
2017-05-12 19:20:09 +0100

    Help wanted please

---
+++ experimental/sandbox/inc/scalemodes.js
@@ -0,0 +1,32 @@
+
+// Can anyone fill in the blanks??
+
+var scaleModes = {
+    major: {
+        ionian:  	[2,2,1,2,2,2,1], // last 1 added for 'wrap around'
+        dorian: 	[2,1,2,2,2,1, "?"],
+        phyrigian:  [1,2,2,2,1,2, "?"],
+        lydian:     [2,2,2,1,2,2, "?"],
+        mixolydian: [2,2,1,2,2,1, "?"],
+        aeolian:    [2,1,2,2,1,2, "?"],
+        locrian:    [1,2,2,1,2,2, "?"]
+    },
+    harmonic_minor: {
+        harmminor: 		[2,1,2,2,1,3, "?"],
+        locriansharpsix:[1,2,2,1,3,1, "?"],
+        ionianaug:     	[2,2,1,3,1,2, "?"],
+        romanian: 		[2,1,3,1,2,1, "?"],
+        phrygiandom:   	[1,3,1,2,1,2, "?"],
+        lydiansharptwo: [3,1,2,1,2,2, "?"],
+        ultralocrian:   [1,2,1,2,2,1, "?"]
+    },
+    melodic_minor: {
+        jazz_minor: 		[2,1,2,2,2,2, "?"],
+        dorianflatnine: 	[1,2,2,2,2,1, "?"],
+        lydianaug: 			[2,2,2,2,1,2, "?"],
+        lydiandom: 			[2,2,2,1,2,1, "?"],
+        mixolydianflatsix: 	[2,2,1,2,1,2, "?"],
+        semilocrian: 		[2,1,2,1,2,2, "?"],
+        superlocrian: 		[1,2,1,2,2,2, "?"]
+    }
+};
